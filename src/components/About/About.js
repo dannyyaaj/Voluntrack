ebe2e978ffd2d3a2abf027e527eb7f153ee8ebff
Danny Yang <dannyyaaj@gmail.com>
2018-08-13 22:25:05 -0500

    center align voluntrack summary

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,10 +3,7 @@ import React, {Component} from 'react';
 class About extends Component {
   render() {
     return (
-      <div>
-      
-        <p className="summary">Imagine organizing and tracking volunteers without frustrating clipboards or sign up sheets to pass around</p>
-      </div>
+        <p className="about">Imagine organizing and tracking volunteers without frustrating clipboards or sign up sheets to pass around</p>
     )
   }
 }
