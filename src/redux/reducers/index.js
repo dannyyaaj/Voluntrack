06a129247ba95a78e804ebd7e4feee5d2e5f78c3
Danny Yang <dannyyaaj@gmail.com>
2018-08-15 13:12:25 -0500

    update reducers inside comebine reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import user from './userReducer';
 import login from './loginReducer';
-import userData from './userDataReducer';
 
 const store = combineReducers({
   user,
-  login,
-  userData
+  login
 });
 
 export default store;
