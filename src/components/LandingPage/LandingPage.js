98cdd3c7f81a3a1afeecd82210c7b07847bf89af
Danny Yang <dannyyaaj@gmail.com>
2018-08-15 10:08:48 -0500

    update static nav

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import About from '../About/About';
 import Carousel from '../Carousel/Carousel';
+import PublicNav from '../PublicNav/PublicNav';
 
 class LandingPage extends Component {
   render() {
     return (
       <div>
+        <PublicNav />
         <Carousel />
         <About />
       </div>
