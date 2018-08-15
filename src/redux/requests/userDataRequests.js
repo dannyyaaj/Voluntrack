8c415ec9a649a42b758415d3d004b00d8ad3db7d
Danny Yang <dannyyaaj@gmail.com>
2018-08-15 13:13:32 -0500

    delete unnecessary modules

--- src/redux/requests/userDataRequests.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export function callUserData() {
-  return axios.get('api/userData')
-    .then(response => response.data)
-    .catch((error) => {
-      throw error.response || error;
-    });
-}
