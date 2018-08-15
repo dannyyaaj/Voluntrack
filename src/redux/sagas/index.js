d226a0d44c096eb488ee1a2458c12b98790338c4
Danny Yang <dannyyaaj@gmail.com>
2018-08-15 13:12:54 -0500

    fix indentation

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -2,7 +2,6 @@ import { all } from 'redux-saga/effects';
 import userSaga from './userSaga';
 import loginSaga from './loginSaga';
 
-
 export default function* rootSaga() {
   yield all([
     userSaga(),
