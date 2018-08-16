c59c03a1316b62a9f71875e8409c72c77933bd90
Danny Yang <dannyyaaj@gmail.com>
2018-08-16 09:45:04 -0500

    redirect users to their dashboard when they click cancel button. Additionally, component is now connected to redux state and its local state is connected to its parent component AdminProfileView

--- src/components/UserAccess/AdminUser/AdminProfile/AdminProfileForm.js
+++ src/components/UserAccess/AdminUser/AdminProfile/AdminProfileForm.js
@@ -6,44 +6,69 @@ import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => ({
+  user: state.user,
+});
 
 const styles = () => ({
-  container: {
+  form: {
     textAlign: 'center',
     margin: '1.5rem auto',
-    padding: '1.5rem 2rem',
     backgroundColor: 'white',
+  },
+  container: {
+    width: '90%'
   },
   formTitle: {
     color: 'black',
     marginBottom: '2.5rem',
   },
   textField: {
-    width: 500,
-    margin: '0 auto',
-    padding: '0 0 1.5rem 0'
+    margin: '.5rem auto',
+    padding: '0 0 1.4rem 0'
   },
-  getStarted: {
-  
+  button: {
+
   }
 });
 
 class AdminProfileForm extends Component {
+
+  goHome = () => {
+    window.location.href = "http://localhost:3000/#/user"
+  }
+
   render() {
+    console.log(this.props)
     return (
       <form
-        className={this.props.classes.container}
+        className={this.props.classes.form}
         onSubmit={this.props.registerUser}>
-        <FormControl component="fieldset">
-          <FormLabel htmlFor="firstName">
+        <FormControl component="fieldset"
+          className={this.props.classes.container}>
+          <FormLabel htmlFor="first_name">
             <TextField
               className={this.props.classes.textField}
               label="First Name"
-              name="firstName"
+              name="first_name"
+              placeholder=""
+              fullWidth
+              value={this.props.user.first_name}
+              onChange={this.props.handleInputChangeFor('first_name')}
+            />
+          </FormLabel>
+          <FormLabel htmlFor="middle_name">
+            <TextField
+              className={this.props.classes.textField}
+              label="Middle Name"
+              name="middle_name"
+              placeholder=""
               fullWidth
               margin="normal"
-              value={this.props.firstName}
-              onChange={this.props.handleInputChangeFor('firstName')}
+              value={this.props.user.middle_name}
+              onChange={this.props.handleInputChangeFor('middle_name')}
             />
           </FormLabel>
           <FormLabel htmlFor="lastName">
@@ -51,7 +76,9 @@ class AdminProfileForm extends Component {
               className={this.props.classes.textField}
               label="Last Name"
               name="lastName"
+              placeholder=""
               fullWidth
+              margin="normal"
               value={this.props.lastName}
               onChange={this.props.handleInputChangeFor('lastName')}
             />
@@ -61,52 +88,96 @@ class AdminProfileForm extends Component {
               className={this.props.classes.textField}
               label="Email"
               name="email"
+              placeholder=""
               fullWidth
               value={this.props.email}
               onChange={this.props.handleInputChangeFor('email')}
             />
           </FormLabel>
-          <FormLabel htmlFor="username">
+          <FormLabel htmlFor="primary_phone"
+            className={this.props.classes.formLabel}>
             <TextField
               className={this.props.classes.textField}
-              label="Username"
-              name="username"
+              label="Primary Phone xxx-xxx-xxxx"
+              name="primary_phone"
+              placeholder=""
               fullWidth
-              value={this.props.username}
-              onChange={this.props.handleInputChangeFor('username')}
+              value={this.props.user.primary_phone}
+              onChange={this.props.handleInputChangeFor('primary_phone')}
             />
           </FormLabel>
-          <FormLabel htmlFor="password">
+          <FormLabel htmlFor="address">
             <TextField
               className={this.props.classes.textField}
-              label="Password"
-              type="password"
-              name="password"
+              label="Address"
+              name="address"
+              placeholder=""
               fullWidth
-              value={this.props.password}
-              onChange={this.props.handleInputChangeFor('password')}
+              value={this.props.user.address}
+              onChange={this.props.handleInputChangeFor('address')}
+            />
+          </FormLabel>
+          <FormLabel htmlFor="city">
+            <TextField
+              className={this.props.classes.textField}
+              label="City"
+              name="city"
+              placeholder=""
+              fullWidth
+              value={this.props.user.city}
+              onChange={this.props.handleInputChangeFor('city')}
+            />
+          </FormLabel>
+          <FormLabel htmlFor="state">
+            <TextField
+              className={this.props.classes.textField}
+              label="State"
+              name="state"
+              placeholder=""
+              fullWidth
+              value={this.props.user.state}
+              onChange={this.props.handleInputChangeFor('state')}
+            />
+          </FormLabel>
+          <FormLabel htmlFor="zipcode">
+            <TextField
+              className={this.props.classes.textField}
+              label="Zipcode"
+              name="zipcode"
+              placeholder=""
+              fullWidth
+              value={this.props.user.zipcode}
+              onChange={this.props.handleInputChangeFor('zipcode')}
             />
           </FormLabel>
           <div>
             <Button
-             className={this.props.classes.getStarted}
+              className={this.props.classes.button}
               color="primary"
               variant="extendedFab"
-              fullWidth="true"
+              fullWidth
               type="submit"
             >
-              Get Started
+              Update Profile
                </Button>
           </div>
           <div>
-          <Link to="/home">Cancel</Link>
+            <Button
+              className={this.props.classes.button}
+              color="secondary"
+              variant="extendedFab"
+              fullWidth
+              onClick={this.goHome}
+            >
+              cancel
+          </Button>
           </div>
         </FormControl>
-      </form>
+      </form >
     )
   }
 }
 
 const StyledAdminProfileForm =
   withStyles(styles)(AdminProfileForm);
-export default StyledAdminProfileForm;
+export default connect(mapStateToProps)(StyledAdminProfileForm)
