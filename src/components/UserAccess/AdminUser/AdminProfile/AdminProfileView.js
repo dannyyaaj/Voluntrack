7bf33883043848f2a737d7f28684fc4c85c482b6
Danny Yang <dannyyaaj@gmail.com>
2018-08-17 11:33:29 -0500

    wait a bit before dispatching request to get user profile data from redux

--- src/components/UserAccess/AdminUser/AdminProfile/AdminProfileView.js
+++ src/components/UserAccess/AdminUser/AdminProfile/AdminProfileView.js
@@ -3,9 +3,11 @@ import { connect } from 'react-redux';
 import AdminProfileForm from './AdminProfileForm';
 import { withStyles } from "@material-ui/core/styles";
 import { Card, CardContent, Grid } from '@material-ui/core';
+import { USER_DATA_ACTIONS } from '../../../../redux/actions/userDataActions';
 
 const mapStateToProps = state => ({
   user: state.user,
+  profile: state.user.profile
 });
 
 const styles = {
@@ -32,25 +34,50 @@ class AdminProfileView extends Component {
       address: '',
       city: '',
       state: '',
-      zipcode: ''
+      zipcode: '',
     };
   }
 
-  updateUserProfile = (event) => {
-    event.preventDefault();
-
+  componentWillMount() {
+    this.props.dispatch({ type: USER_DATA_ACTIONS.FETCH_USER_DATA });
   }
 
-  handleInputChangeFor = propertyName => (event) => {
+  // Wait for reduxState to load, then set the local state's values
+  async componentDidMount() {
+    await new Promise(resolve => { setTimeout(resolve, 50) })
+    this.setValues();
+  }
+
+  componentWillUnmount() {
+    this.props.dispatch({ type: USER_DATA_ACTIONS.UNSET_USER_DATA });
+  }
+
+  setValues = () => {
+    const oldFirstName = this.props.profile.first_name;
+    const oldMiddleName = this.props.profile.middle_name;
+    const oldLastName = this.props.profile.last_name;
+    const oldEmail = this.props.profile.email;
+    const oldPrimaryPhone = this.props.profile.primary_phone;
+    const oldAddress = this.props.profile.address;
+    const oldCity = this.props.profile.city;
+    const oldState = this.props.profile.state;
+    const oldZipcode = this.props.profile.zipcode;
+
+    // Sets local state's values to the most current user information in database, which is stored in reduxState
     this.setState({
-      [propertyName]: event.target.value,
-    });
+      first_name: oldFirstName,
+      middle_name: oldMiddleName,
+      last_name: oldLastName,
+      email: oldEmail,
+      primary_phone: oldPrimaryPhone,
+      address: oldAddress,
+      city: oldCity,
+      state: oldState,
+      zipcode: oldZipcode,
+    })
   }
-
-
 
   render() {
-    console.log(this.state, 'local state')
     return (
       <div>
         <Grid container justify="center">
@@ -60,8 +87,7 @@ class AdminProfileView extends Component {
                 <h1 className="formHeader">Your Profile</h1>
                 <p className="formDescription">Manage your basic information below - name, email, and phone number - to make it easier for organizations to get in touch.</p>
                 <AdminProfileForm
-                  user={this.state}
-                  handleInputChangeFor={this.handleInputChangeFor}
+                  profile={this.state}
                 />
               </CardContent>
             </Card>
@@ -71,6 +97,7 @@ class AdminProfileView extends Component {
     );
   }
 }
+
 const StyledAdminProfileView = withStyles(styles)(AdminProfileView)
 
 export default connect(mapStateToProps)(StyledAdminProfileView);
