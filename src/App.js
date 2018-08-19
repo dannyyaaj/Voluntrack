ef700380d92993304ca3a05f313f60dc949834ee
Danny Yang <dannyyaaj@gmail.com>
2018-08-19 04:27:59 -0500

    update ManageEventsPage file path

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import InfoPage from './components/InfoPage/InfoPage';
 import ProfilePage from './components/ProfilePage/ProfilePage';
 import ErrorNotFound from './components/ErrorNotFound/ErrorNotFound';
 import './styles/main.css';
-import ManageEvents from './components/UserAccess/AdminUser/ManageEvents/ManageEventsPage';
+import ManageEventsPage from './components/UserAccess/AdminUser/ManageEvents/ManageEventsPage';
 
 const App = () => (
   <div>
@@ -46,7 +46,7 @@ const App = () => (
           />
           <Route
             exact path="/events"
-            component={ManageEvents}
+            component={ManageEventsPage}
           />
           <Route
             exact path="/profile"
