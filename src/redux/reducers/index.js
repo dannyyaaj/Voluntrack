1ed904521482c8303da6ed5016086024e0314412
Danny Yang <dannyyaaj@gmail.com>
2018-08-20 19:03:18 -0500

    add event reducer inside combine reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import user from './userReducer';
 import login from './loginReducer';
+import event from './eventReducer';
 
 const store = combineReducers({
   user,
-  login
+  login,
+  event
 });
 
 export default store;
