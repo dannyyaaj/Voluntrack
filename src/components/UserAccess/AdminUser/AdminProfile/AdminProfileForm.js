976aa7e65f0e65283d95ed58ce69aba5b0121788
Danny Yang <dannyyaaj@gmail.com>
2018-08-23 00:04:35 -0500

    remove Grid component and add InputLabelProps to text field inputs

--- src/components/UserAccess/AdminUser/AdminProfile/AdminProfileForm.js
+++ src/components/UserAccess/AdminUser/AdminProfile/AdminProfileForm.js
@@ -3,8 +3,7 @@ import { connect } from 'react-redux';
 // material ui components
 import { withStyles, FormControl, FormLabel, TextField, Button } from '@material-ui/core';
 // react material responsive grid components
-import { Grid, Row, Col } from 'react-material-responsive-grid';
-
+import { Row, Col } from 'react-material-responsive-grid';
 
 const mapStateToProps = state => ({
   user: state.user,
@@ -32,12 +31,7 @@ const styles = () => ({
 
   }
 });
-
 class AdminProfileForm extends Component {
-  constructor(props) {
-    super(props);
-  }
-
 
   goHome = () => {
     window.location.href = "http://localhost:3000/#/user"
@@ -45,156 +39,182 @@ class AdminProfileForm extends Component {
 
   render() {
     return (
-        <form
-          className={this.props.classes.form}
-          onSubmit={this.props.handleClickOpen}>
-          <FormControl component="fieldset"
-            className={this.props.classes.container}>
-            <Row>
-              <Col xs4={2} md={6} lg={6}>
-                <FormLabel htmlFor="first_name">
-                  <TextField
-                    className={this.props.classes.textField}
-                    label="First Name"
-                    name="first_name"
-                    placeholder={this.props.profile.first_name}
-                    fullWidth
-                    defaultValue={this.props.profile.first_name}
-                    onChange={this.props.handleInputChangeFor('first_name')}
-                  />
-                </FormLabel>
-              </Col>
-              <Col xs4={2} md={6} lg={6}>
-                <FormLabel htmlFor="middle_name">
-                  <TextField
-                    className={this.props.classes.textField}
-                    label="Middle Name"
-                    name="middle_name"
-                    placeholder={this.props.profile.middle_name}
-                    fullWidth
-                    margin="normal"
-                    defaultValue={this.props.profile.middle_name}
-                    onChange={this.props.handleInputChangeFor('middle_name')}
-                  />
-                </FormLabel>
-              </Col>
-            </Row>
-            <Row>
-              <Col xs4={2} md={6} lg={6}>
-                <FormLabel htmlFor="lastName">
-                  <TextField
-                    className={this.props.classes.textField}
-                    label="Last Name"
-                    name="last_name"
-                    placeholder={this.props.profile.last_name}
-                    fullWidth
-                    onChange={this.props.handleInputChangeFor('last_name')}
-                  />
-                </FormLabel>
-              </Col>
-              <Col xs4={2} md={6} lg={6}>
-                <FormLabel htmlFor="email">
-                  <TextField
-                    className={this.props.classes.textField}
-                    label="Email"
-                    name="email"
-                    placeholder={this.props.profile.email}
-                    fullWidth
-                    onChange={this.props.handleInputChangeFor('email')}
-                  />
-                </FormLabel>
-              </Col>
-            </Row>
-            <Row>
-              <Col xs4={2} md={6} lg={6}>
-                <FormLabel htmlFor="primary_phone"
-                  className={this.props.classes.formLabel}>
-                  <TextField
-                    className={this.props.classes.textField}
-                    label="Primary Phone xxx-xxx-xxxx"
-                    name="primary_phone"
-                    placeholder={this.props.profile.primary_phone}
-                    fullWidth
-                    onChange={this.props.handleInputChangeFor('primary_phone')}
-                  />
-                </FormLabel>
-              </Col>
-              <Col xs4={2} md={6} lg={6}>
-                <FormLabel htmlFor="address">
-                  <TextField
-                    className={this.props.classes.textField}
-                    label="Address"
-                    name="address"
-                    placeholder={this.props.profile.address}
-                    fullWidth
-                    onChange={this.props.handleInputChangeFor('address')}
-                  />
-                </FormLabel>
-              </Col>
-            </Row>
-            <Row>
-              <Col xs4={2} md={6} lg={6}>
-
-                <FormLabel htmlFor="city">
-                  <TextField
-                    className={this.props.classes.textField}
-                    label="City"
-                    name="city"
-                    placeholder={this.props.profile.city}
-                    fullWidth
-                    onChange={this.props.handleInputChangeFor('city')}
-                  />
-                </FormLabel>
-              </Col>
-              <Col xs4={2} md={6} lg={6}>
-                <FormLabel htmlFor="state">
-                  <TextField
-                    className={this.props.classes.textField}
-                    label="State"
-                    name="state"
-                    placeholder={this.props.profile.state}
-                    fullWidth
-                    onChange={this.props.handleInputChangeFor('state')}
-                  />
-                </FormLabel>
-              </Col>
-            </Row>
-            <Row>
-              <FormLabel htmlFor="zipcode">
+      <form
+        className={this.props.classes.form}
+        onSubmit={this.props.handleClickOpen}>
+        <FormControl component="fieldset"
+          className={this.props.classes.container}>
+          <Row>
+            <Col xs4={2} md={6} lg={6}>
+              <FormLabel htmlFor="first_name">
                 <TextField
                   className={this.props.classes.textField}
-                  label="Zipcode"
-                  name="zipcode"
-                  placeholder={this.props.profile.zipcode}
+                  label="First Name"
+                  name="first_name"
+                  placeholder={this.props.profile.first_name}
                   fullWidth
-                  onChange={this.props.handleInputChangeFor('zipcode')}
+                  defaultValue={this.props.profile.first_name}
+                  onChange={this.props.handleInputChangeFor('first_name')}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
                 />
               </FormLabel>
-            </Row>
-            <div>
-              <Button
-                className={this.props.classes.button}
-                color="primary"
-                variant="extendedFab"
+            </Col>
+            <Col xs4={2} md={6} lg={6}>
+              <FormLabel htmlFor="middle_name">
+                <TextField
+                  className={this.props.classes.textField}
+                  label="Middle Name"
+                  name="middle_name"
+                  placeholder={this.props.profile.middle_name}
+                  fullWidth
+                  margin="normal"
+                  defaultValue={this.props.profile.middle_name}
+                  onChange={this.props.handleInputChangeFor('middle_name')}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              </FormLabel>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs4={2} md={6} lg={6}>
+              <FormLabel htmlFor="lastName">
+                <TextField
+                  className={this.props.classes.textField}
+                  label="Last Name"
+                  name="last_name"
+                  placeholder={this.props.profile.last_name}
+                  fullWidth
+                  onChange={this.props.handleInputChangeFor('last_name')}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              </FormLabel>
+            </Col>
+            <Col xs4={2} md={6} lg={6}>
+              <FormLabel htmlFor="email">
+                <TextField
+                  className={this.props.classes.textField}
+                  label="Email"
+                  name="email"
+                  placeholder={this.props.profile.email}
+                  fullWidth
+                  onChange={this.props.handleInputChangeFor('email')}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              </FormLabel>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs4={2} md={6} lg={6}>
+              <FormLabel htmlFor="primary_phone"
+                className={this.props.classes.formLabel}>
+                <TextField
+                  className={this.props.classes.textField}
+                  label="Primary Phone xxx-xxx-xxxx"
+                  name="primary_phone"
+                  placeholder={this.props.profile.primary_phone}
+                  fullWidth
+                  onChange={this.props.handleInputChangeFor('primary_phone')}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              </FormLabel>
+            </Col>
+            <Col xs4={2} md={6} lg={6}>
+              <FormLabel htmlFor="address">
+                <TextField
+                  className={this.props.classes.textField}
+                  label="Address"
+                  name="address"
+                  placeholder={this.props.profile.address}
+                  fullWidth
+                  onChange={this.props.handleInputChangeFor('address')}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              </FormLabel>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs4={2} md={6} lg={6}>
+              <FormLabel htmlFor="city">
+                <TextField
+                  className={this.props.classes.textField}
+                  label="City"
+                  name="city"
+                  placeholder={this.props.profile.city}
+                  fullWidth
+                  onChange={this.props.handleInputChangeFor('city')}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              </FormLabel>
+            </Col>
+            <Col xs4={2} md={6} lg={6}>
+              <FormLabel htmlFor="state">
+                <TextField
+                  className={this.props.classes.textField}
+                  label="State"
+                  name="state"
+                  placeholder={this.props.profile.state}
+                  fullWidth
+                  onChange={this.props.handleInputChangeFor('state')}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              </FormLabel>
+            </Col>
+          </Row>
+          <Row>
+            <FormLabel htmlFor="zipcode">
+              <TextField
+                className={this.props.classes.textField}
+                label="Zipcode"
+                name="zipcode"
+                placeholder={this.props.profile.zipcode}
                 fullWidth
-                type="submit"
-              >
-                Update Profile
+                onChange={this.props.handleInputChangeFor('zipcode')}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </FormLabel>
+          </Row>
+          <div>
+            <Button
+              className={this.props.classes.button}
+              color="primary"
+              variant="extendedFab"
+              fullWidth
+              type="submit"
+            >
+              Update Profile
                </Button>
-            </div>
-            <div>
-              <Button
-                className={this.props.classes.button}
-                color="secondary"
-                variant="extendedFab"
-                fullWidth
-                onClick={this.goHome}
-              >
-                cancel
+          </div>
+          <div>
+            <Button
+              className={this.props.classes.button}
+              color="secondary"
+              variant="extendedFab"
+              fullWidth
+              onClick={this.goHome}
+            >
+              cancel
           </Button>
-            </div>
-          </FormControl>
-        </form>
+          </div>
+        </FormControl>
+      </form>
     )
   }
 }
