69ce15972c9d0868b2a36d19b6ae0085b45c717b
Danny Yang <dannyyaaj@gmail.com>
2018-08-23 14:53:08 -0500

    update GET request query and add in PUT request

--- server/routes/event.router.js
+++ server/routes/event.router.js
@@ -6,7 +6,8 @@ const router = express.Router();
 router.get('/upcoming', rejectUnauthenticated, (req, res) => {
   // GET request for upcoming events
   const queryText = `SELECT * FROM "event"
-  WHERE ("date" >= CURRENT_DATE);`;
+  WHERE ("date" >= CURRENT_DATE)
+  ORDER BY "date" ASC;`;
 
   pool.query(queryText)
     .then((result) => {
@@ -14,6 +15,26 @@ router.get('/upcoming', rejectUnauthenticated, (req, res) => {
     })
     .catch((error) => {
       console.log(error);
+      res.sendStatus(500);
+    })
+});
+
+router.put('/upcoming/:id', rejectUnauthenticated, (req, res) => {
+  // PUT request to update event information
+  const newEventData = req.body
+  console.log(req, 'requesttttt')
+  console.log(req.params.id, 'id params')
+  console.log(req.body, 'req body')
+  const queryText = `UPDATE "event" SET "name" = $2, "address" = $3, "city" = $4, "state" = $5, "zipcode" = $6, "coordinator" = $7, "date" = $8, "start_time" = $9, "end_time" = $10, "description" = $11, "num_of_volunteers" = $12, "image_url" = $13, "roles" = $14
+  WHERE "id" = $1;`;
+
+  const serializedData = [req.params.id, newEventData.name, newEventData.address, newEventData.city, newEventData.state, newEventData.zipcode, newEventData.coordinator, newEventData.date, newEventData.start_time, newEventData.end_time, newEventData.description, newEventData.num_of_volunteers, newEventData.image_url, newEventData.roles];
+
+  pool.query(queryText, serializedData)
+    .then((result) => {
+      res.sendStatus(201);
+    })
+    .catch((error) => {
       res.sendStatus(500);
     })
 });
@@ -40,7 +61,7 @@ router.post('/', rejectUnauthenticated, (req, res) => {
 
   const queryText = `INSERT INTO "event" ("name", "address", "city", "state", "zipcode", "coordinator", "date", "start_time", "end_time", "description", "num_of_volunteers", "image_url","roles","org_id") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9 ,$10, $11, $12, $13, 1);`;
 
-  const serializedData = [newEventData.name, newEventData.address, newEventData.city, newEventData.state, newEventData.zipcode, newEventData.coordinator, newEventData.date,newEventData.start_time, newEventData.end_time, newEventData.description, newEventData.num_of_volunteers, newEventData.image_url, newEventData.roles]
+  const serializedData = [newEventData.name, newEventData.address, newEventData.city, newEventData.state, newEventData.zipcode, newEventData.coordinator, newEventData.date, newEventData.start_time, newEventData.end_time, newEventData.description, newEventData.num_of_volunteers, newEventData.image_url, newEventData.roles]
 
   pool.query(queryText, serializedData)
     .then((results) => {
@@ -50,5 +71,16 @@ router.post('/', rejectUnauthenticated, (req, res) => {
       res.sendStatus(500);
     })
 });
+
+router.delete('/upcoming/:id', rejectUnauthenticated, (req, res) => {
+  let queryText = `DELETE FROM "event" WHERE "id" = $1;`;
+  pool.query(queryText, [req.params.id])
+    .then((results) => {
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      res.sendStatus(500);
+    })
+})
 
 module.exports = router;
