993c8524fdf3e996ec3e4ca65c054ec7bc95b42d
Danny Yang <dannyyaaj@gmail.com>
2018-08-26 12:32:36 -0500

    create volunteer requests

---
+++ src/redux/requests/volunteerRequests.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+// export function addEvent(newEventData) {
+//   return axios.post('api/event', newEventData)
+//     .then(response => response.data)
+//     .catch((error) => {
+//       throw error.response || error
+//     });
+// }
+
+export function callAllVolunteers() {
+  return axios.get('api/volunteers')
+  .then(response => response.data)
+  .catch((error) => { throw error.response || error});
+}
+
+// export function callPastEvent() {
+//   return axios.get('api/event/past')
+//   .then(response => response.data)
+//   .catch((error) => { throw error.response || error});
+// }
+
+// export function updateEventData(payload) {
+//   return axios.put(`api/event/upcoming/${payload[0]}`, payload[1])
+//     .then(response => response.data)
+//     .catch((error) => { throw error.response || error; });
+// }
+// export function deleteEvent(payload) {
+//   return axios.delete(`api/event/upcoming/${payload}`)
+//     .then(response => response.data)
+//     .catch((error) => { throw error.response || error; });
+// }
+
