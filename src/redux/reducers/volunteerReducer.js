c4289fd42ec3a5f8e440e91388444f034e1a071f
Danny Yang <dannyyaaj@gmail.com>
2018-08-26 12:31:30 -0500

    create volunteer reducer

---
+++ src/redux/reducers/volunteerReducer.js
@@ -0,0 +1,26 @@
+import { combineReducers } from 'redux';
+import { VOLUNTEER_ACTIONS } from '../actions/volunteerActions';
+
+
+// Dispatch action type to get all volunteers
+const allVolunteers = (state = null, action) => {
+  switch (action.type) {
+    case VOLUNTEER_ACTIONS.SET_ALL_VOLUNTEERS:
+      return action.payload || state;
+    default:
+      return state;
+  }
+}
+
+// const eventVolunteers = (state = null, action) => {
+//   switch (action.type) {
+//     case EVENT_ACTIONS.SET_PAST_EVENT:
+//       return action.payload || state;
+//     default:
+//       return state;
+//   }
+// }
+
+export default combineReducers({
+ allVolunteers
+});
