a3cf8a67127f3045808d85461ba8013d779bd69c
Danny Yang <dannyyaaj@gmail.com>
2018-08-27 15:22:22 -0500

    created custom authorization middleware

---
+++ server/modules/authorization-middleware.js
@@ -0,0 +1,13 @@
+const rejectUnauthorized = (req, res, next) => {
+  // check if logged in
+  if (req.user.admin_access == true) {
+    // They were authenticated! User may do the next thing
+    // Note! They may not be Authorized to do all things
+    next();
+  } else {
+    // failure best handled on the server. do redirect here.
+    res.sendStatus(403);
+  }
+};
+
+module.exports = { rejectUnauthorized };
