da9b21bbf4e6dc36454a9a55f4f293b2db0b953d
Danny Yang <dannyyaaj@gmail.com>
2018-09-01 22:45:10 -0500

    import PublicErrorMessage to reduce code clutter

--- src/components/ErrorNotFound/ErrorNotFound.js
+++ src/components/ErrorNotFound/ErrorNotFound.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { USER_ACTIONS } from '../../redux/actions/userActions';
+import PublicErrorMessage from './PublicErrorMessage';
 import { connect } from 'react-redux';
 
 const mapStateToProps = state => ({
@@ -11,33 +12,18 @@ class ErrorNotFound extends Component {
     this.props.dispatch({ type: USER_ACTIONS.FETCH_USER });
   }
 
-  // redirect user to home page
+  // redirect user to landing or events page
   goHome = () => this.props.history.push('/');
 
-  goUser = () => this.props.history.push('/user')
+  goUser = () => this.props.history.push('/events')
 
   render() {
-    let content = null;
-    if (this.props.user.userName) {
-      content = (
-        <div>
-          <h1>404</h1>
-          <p>OOPS, SORRY WE CAN'T FIND THAT PAGE!</p>
-          <button onClick={this.goUser}>Home</button>
-        </div>
-      )
-    } else {
-      content = (
-        <div>
-          <h1>404</h1>
-          <p>OOPS, SORRY WE CAN'T FIND THAT PAGE!</p>
-          <button onClick={this.goHome}>Home Page</button>
-        </div>
-      )
-    }
     return (
-      <div>
-        {content}
+      <div className="errorMessage">
+        <PublicErrorMessage
+          goUser={this.goUser}
+          goHome={this.goHome}
+        />
       </div>
     )
   }
