1c959947c499ffb967ace23e083b8c143717d8ab
Danny Yang <dannyyaaj@gmail.com>
2018-09-01 22:56:15 -0500

    Style landing page inside grid and update PublicNav file path

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 import About from '../About/About';
 import Carousel from '../Carousel/Carousel';
-import PublicNav from '../PublicNav/PublicNav';
+import PublicNav from '../Nav/PublicNav';
+import { Grid, Row, Col } from 'react-material-responsive-grid';
 
 class LandingPage extends Component {
   render() {
     return (
-      <div>
-        <PublicNav />
-        <Carousel />
-        <About />
-      </div>
+      <Grid>
+        <Row>
+          <Col xs4={4} md={12} lg={12}>
+            <PublicNav />
+          </Col>
+        </Row>
+        <Row>
+          <Col xs4={4} md={12} lg={12}>
+            <Carousel />
+          </Col>
+        </Row>
+        <Row>
+          <Col xs4={4} md={12} lg={12}>
+            <About />
+          </Col>
+        </Row>
+      </Grid>
     )
   }
 }
