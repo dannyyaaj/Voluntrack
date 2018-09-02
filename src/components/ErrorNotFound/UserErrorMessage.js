4c70b0106a6acf17cdb8ea818177a53ac7d61dac
Danny Yang <dannyyaaj@gmail.com>
2018-09-01 22:51:38 -0500

    update user error message

--- src/components/ErrorNotFound/UserErrorMessage.js
+++ src/components/ErrorNotFound/UserErrorMessage.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Button } from '@material-ui/core';
 
 
 class UserErrorMessage extends Component {
 
   // redirect user to home page
-  goUser = () => this.props.history.push('/user')
+  goUser = () => this.props.history.push('/events')
 
   render() {
     let content = (
       <div>
         <h1>404</h1>
         <p>OOPS, SORRY WE CAN'T FIND THAT PAGE!</p>
-        <button onClick={this.goUser}>Home</button>
+        <Button
+          color="primary"
+          variant="extendedFab"
+          onClick={this.goUser}>
+          Home
+        </Button>
       </div>
     )
     return (
-      <div>
+      <React.Fragment>
         {content}
-      </div>
+      </React.Fragment>
     )
   }
 }
